69360effd4a4b3c727eb095a19e1d6ec27f2a4eb
NandiniDoradla <doradlanandini@gmail.com>
2025-07-19 19:44:18 +0530

    Delete ProjectFiles directory

--- ProjectFiles/adminRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const adminRouter = express.Router();
-const { adminLogin } = require('../controllers/adminController');
-
-adminRouter.post('/login', adminLogin);
-
-module.exports = adminRouter;
